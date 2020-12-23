dbb624e7dd180544b1044eec7ee6b9020a41fec3
maxlep <mlepkows@stevens.edu>
2020-12-23 01:12:46 -0500

    Multiple cubes.

---
+++ Assets/MyAssets/Scripts/Feedbacks/FeedbacksOnImpact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class FeedbacksOnImpact : MonoBehaviour
+{
+    [SerializeField] private MMFeedbacks feedbacks;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        feedbacks.PlayFeedbacks();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        feedbacks.PlayFeedbacks();
+    }
+}
---
+++ Assets/MyAssets/Scripts/Interactions/SimpleShoot.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace MyAssets.Scripts.Interactions
+{
+    public class SimpleShoot : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private float shootForce;
+        [SerializeField] private Transform firePoint;
+
+        public void DoShoot()
+        {
+            var instantiated = PhotonNetwork.Instantiate("Object/Network Cube", firePoint.position, firePoint.rotation);
+            var rb = instantiated.GetComponent<Rigidbody>();
+            rb.velocity = firePoint.forward * shootForce;
+        }
+    }
+}
