42ef0f10d58072f55b300e59a77960820ae5b398
Dan C <djc5627@gmail.com>
2020-12-26 21:49:05 -0500

    Remove clutter from main scene. Add tooltips with player index.

---
+++ Assets/MyAssets/Scripts/Networking/NetworkTooltip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkTooltip : MonoBehaviour
+{
+    [SerializeField] private PhotonView photonView;
+    [SerializeField] private Text tooltipText;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        tooltipText.text = GetTooltipLabel();
+    }
+
+    private string GetTooltipLabel()
+    {
+        var owner = photonView.Owner;
+        
+        if (owner == null)
+            return "";
+
+        string ownerNickname = owner.NickName;
+
+        if (ownerNickname == null)
+            return "";
+
+        return ownerNickname; 
+    }
+}
